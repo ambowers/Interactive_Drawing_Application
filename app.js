9b6d87ede6c00020af1ce71864ad65d7b316a90a
lina <ambowers@usf.edu>
2024-10-26 17:19:10 -0400

    Implement shape drawing logic for lines, rectangles, and circles

--- app.js
+++ app.js
@@ -19,3 +19,33 @@ shapeOptions.forEach(option => {
         selectedShape = option.value
     });
 });
+
+
+// Task 3 Implement shape drawing logic
+
+canvas.addEventListener('mousedown', (event) =>{
+    drawing = true
+    initialX= event.offsetX;
+    initialY = event.offsetY;
+});
+
+canvas.addEventListener('mousemove',(event)=> {
+    if (!drawing) return;
+    const currentX = event.offsetX;
+    const currentY = event.offsetY;
+
+    ctx.clearRect(0,0, canvas.clientWidth, canvas.height);
+    ctx.drawColor = selectedColor
+
+    if(selectedShape ='line'){
+        drawLine(initialX, initialY, currentX, currentY);
+    } else if (selectedShape === 'rectangle'){
+        drawRectangle(initialX, initialY, currentX - initialX, currentY - initialY);
+    } else if (selectedShape = 'circle'){
+        const radius = Math.sqrt(Math.pow(currentX - initialX, 2)+ Math.pow(currentY - initialY, 2));
+        drawCircle(initialX, initialY, radius);
+    }});
+
+canvas.addEventListener('mouseup', ()=>{
+    ctx.clearRect(0,0, canvas.clientWidth, canvas.height);
+});
