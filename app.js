c7d944249c1631a38ab919fccd75822ae79d626c
lina <ambowers@usf.edu>
2024-10-27 13:20:57 -0400

    Implement color selection and canvas clearing functionality

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 // Task 2 Configure the javascript for drawing context
-
 const canvas= document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const clearButton = document.getElementById('clear');
-const colorSelection = document.getElementById('clear');
+const colorSelection = document.getElementById('color');
 const shapeOptions = document.getElementById('shape');
 
 //set up drawing
@@ -22,7 +21,6 @@ shapeOptions.forEach(option => {
 
 
 // Task 3 Implement shape drawing logic
-
 canvas.addEventListener('mousedown', (event) =>{
     drawing = true
     initialX= event.offsetX;
@@ -35,17 +33,49 @@ canvas.addEventListener('mousemove',(event)=> {
     const currentY = event.offsetY;
 
     ctx.clearRect(0,0, canvas.clientWidth, canvas.height);
-    ctx.drawColor = selectedColor
+    ctx.strokeStyle = selectedColor
 
-    if(selectedShape ='line'){
+    if (selectedShape ==='line'){
         drawLine(initialX, initialY, currentX, currentY);
     } else if (selectedShape === 'rectangle'){
         drawRectangle(initialX, initialY, currentX - initialX, currentY - initialY);
-    } else if (selectedShape = 'circle'){
+    } else if (selectedShape === 'circle'){
         const radius = Math.sqrt(Math.pow(currentX - initialX, 2)+ Math.pow(currentY - initialY, 2));
         drawCircle(initialX, initialY, radius);
     }});
 
 canvas.addEventListener('mouseup', ()=>{
-    ctx.clearRect(0,0, canvas.clientWidth, canvas.height);
+    drawing = false;
 });
+//function for drawing a line
+function drawLine( x1, y1, x2, y2){
+    ctx.beginPath();
+    ctx.moveTo(x1,y1)
+    ctx.lineTo(x2,y2);
+    ctx.stroke();
+}
+
+//function for drawing a rectangle
+function drawRectangle(x,y,width,height){
+    ctx.beginPath();
+    ctx.rect(x,y,width,height);
+    ctx.stroke();
+}
+
+//function for drawing a circle 
+function drawCircle(x,y,radius){
+    ctx.beginPath();
+    ctx.arc(x,y, radius, 0,2*Math.PI) //draws full circle (2pir)
+    ctx.stroke();
+}
+
+
+// Task 4 add color selection and canvas clearing
+colorSelection.addEventListener('input',(event)=>{
+    selectedColor=event.target.value;
+})
+
+clearButton.addEventListener('click', ()=> {
+    ctx.clearRect(0,0, canvas.width,canvas.height);
+});
+
